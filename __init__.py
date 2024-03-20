238df2793d6edbb6aae5da39955d3e770f8336c5
Atsada Choichueadee <atsada.choichueadee@protonmail.com>
2024-03-21 02:50:12 +0700

    update for blender 4.0

--- __init__.py
+++ __init__.py
@@ -1,40 +1,40 @@
 bl_info = {
     "name": "Minecraft Model Json Exporter",
     "author": "Yesssssman",
-    "blender": (2, 80, 0),
+    "blender": (4, 0, 0),
     "category": "Import-Export",
     "location": "File > Import-Export",
-    "description":  "Specially designed exporter for developing Minecraft Epic Fight Mod"
+    "description": "Specially designed exporter for developing Minecraft Epic Fight Mod",
 }
 
 import bpy
-from bpy.props import *
+
 from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
 
-class ExportToJson(bpy.types.Operator, ExportHelper):
+
+class ExportToJson(Operator, ExportHelper):
     """Export to Json that specially designed for Epic Fight"""
-    bl_idname = "export_mc.json"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Export to Json for Minecraft"         # Display name in the interface.
+
+    bl_idname = "export_mc.json"
+    bl_label = "Export to Json for Minecraft"
     filename_ext = ".json"
-    filter_glob = StringProperty(default="*.json", options={"HIDDEN"})
+    filter_glob: StringProperty(default="*.json", options={"HIDDEN"})
 
-    @classmethod
-    def poll( cls, context ):
-        return context.active_object != None
-
-    export_anim = BoolProperty(
+    export_anim: BoolProperty(
         name="Export Animation",
         description="Export animation data",
         default=True,
     )
 
-    export_mesh = BoolProperty(
+    export_mesh: BoolProperty(
         name="Export Mesh",
         description="Export mesh data",
         default=True,
     )
 
-    export_armature = BoolProperty(
+    export_armature: BoolProperty(
         name="Export Armature",
         description="Export armature data",
         default=True,
@@ -42,23 +42,30 @@ class ExportToJson(bpy.types.Operator, ExportHelper):
 
     def execute(self, context):
         if not self.filepath:
-            raise Exception("filepath not set")    
+            raise Exception("filepath not set")
         keywords = self.as_keywords()
-        print('keyward is ', keywords)
-        
+        print("keyward is ", keywords)
+
         from . import export_mc_json
+
         return export_mc_json.save(context, **keywords)
 
-def menu_func( self, context ):
-    self.layout.operator(ExportToJson.bl_idname, text="Animated Minecraft Model (.json)")
+
+def menu_func_export(self, context):
+    self.layout.operator(
+        ExportToJson.bl_idname, text="Animated Minecraft Model (.json)"
+    )
+
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_export.append(menu_func)
+    bpy.utils.register_class(ExportToJson)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
 
 def unregister():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_export.remove(menu_func)
+    bpy.utils.unregister_class(ExportToJson)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+
 
 if __name__ == "__main__":
     register()
